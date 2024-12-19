19330a5f212cca5ab8e3f0bc916ce5e1295e9d56
Katjana Kosic <ge56yuw@tum.de>
2024-12-19 19:57:46 +0100

    Extract SearchBar as component

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,71 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material"
+import React from "react"
+import { useNavigate } from "react-router-dom"
+
+export const SearchBar = () => {
+  const [username, setUsername] = React.useState("")
+  const navigate = useNavigate()
+
+  const handleSearch = () => {
+    if (!username.trim()) return
+    navigate(`/repositories/${username.trim()}`)
+  }
+
+  return (
+    <Box display="flex" flexDirection="row" gap={3}>
+      <TextField
+        fullWidth
+        label="Username"
+        variant="outlined"
+        color="primary"
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            color: "#e0e0e0",
+            "& .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#e0e0e0",
+            },
+            "&.Mui-focused": {
+              "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "primary.main",
+              },
+            },
+            "& .MuiInputLabel-outlined": {
+              color: "#e0e0e0",
+              "&.Mui-focused": {
+                color: "primary.main",
+              },
+            },
+          },
+        }}
+        InputLabelProps={{
+          sx: {
+            color: "#e0e0e0",
+            "&.Mui-focused": {
+              color: "primary.main",
+            },
+          },
+        }}
+        required
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleSearch()
+        }}
+      />
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "#5991f1",
+          color: "#FFFFFF",
+          "&:hover": {
+            backgroundColor: "#78a6f5",
+          },
+          minWidth: "100px",
+        }}
+        onClick={handleSearch}
+      >
+        Search
+      </Button>
+    </Box>
+  )
+}
